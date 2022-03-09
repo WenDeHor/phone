0a604fc9f48c4b73b414dad4f6fd5de3c3fd6cec
WenDeHor <WenDeHor>
2022-03-09 19:46:36 +0200

    1 commit

---
+++ src/main/java/com/example/phone/ListeningWav.java
@@ -0,0 +1,34 @@
+package com.example.phone;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.sun.media.BasicPlayer;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.embed.swing.JFXPanel;
+
+import javax.media.Manager;
+import javax.media.MediaLocator;
+import javax.media.NoPlayerException;
+import javax.sound.sampled.*;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javazoom.jl.player.Player;
+import org.springframework.stereotype.Component;
+import sun.audio.AudioStream;
+
+@Component
+public class ListeningWav {
+    static String URL = "C:\\FATHER\\phone\\src\\main\\resources\\RecordAudio.wav";
+
+    public void startListening()  {
+        new JFXPanel();
+        String u = new File(URL).toURI().toString();
+        new MediaPlayer(new Media(u)).play();
+    }
+}
---
+++ src/main/java/com/example/phone/controller/MainController.java
@@ -0,0 +1,47 @@
+package com.example.phone.controller;
+
+import com.example.phone.ListeningWav;
+import com.example.phone.SoundRecorder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    private final SoundRecorder soundRecorder;
+    private final ListeningWav listeningWav;
+
+    public MainController(SoundRecorder soundRecorder, ListeningWav listeningWav) {
+        this.soundRecorder = soundRecorder;
+        this.listeningWav = listeningWav;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "mine-page";
+    }
+
+    @GetMapping("/call")
+    public String call(Model model) {
+        setRecord();
+        return "redirect:/";
+    }
+    @GetMapping("/voice")
+    public String voice(Model model) {
+        getVoice();
+        return "redirect:/";
+    }
+
+    @Async
+    public void getVoice() {
+        listeningWav.startListening();
+    }
+
+    @Async
+    public void setRecord() {
+        soundRecorder.startRecord();
+    }
+}
+
+

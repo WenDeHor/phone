549feb47a0a0a70b163195fb96fb77149ce19b6d
WenDeHor <WenDeHor>
2022-03-10 10:35:05 +0200

    5.1 upload - test

--- src/main/java/com/example/phone/MainController.java
+++ src/main/java/com/example/phone/MainController.java
@@ -1,18 +1,26 @@
 package com.example.phone;
 
+import com.example.phone.model.Phone;
+import com.example.phone.repository.PhoneRepository;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class MainController {
     private final SoundRecorder soundRecorder;
     private final ListeningWav listeningWav;
+    private final PhoneRepository phoneRepository;
 
-    public MainController(SoundRecorder soundRecorder, ListeningWav listeningWav) {
+    public MainController(SoundRecorder soundRecorder, ListeningWav listeningWav, PhoneRepository phoneRepository) {
         this.soundRecorder = soundRecorder;
         this.listeningWav = listeningWav;
+        this.phoneRepository = phoneRepository;
     }
 
     @GetMapping("/")
@@ -20,23 +28,27 @@ public class MainController {
         return "mine-page";
     }
 
-    @GetMapping("/call")
-    public String call(Model model) {
+    @PostMapping("/call")
+    public String call(Model model, MultipartFile file) throws IOException {
+//        Phone phone = new Phone();
+//        phone.setVolume(file.getBytes());
+//        phoneRepository.save(phone);
         setRecord();
         return "redirect:/";
     }
+
     @GetMapping("/voice")
     public String voice(Model model) {
         getVoice();
         return "redirect:/";
     }
 
-    @Async
+
     public void getVoice() {
         listeningWav.startListening();
     }
 
-    @Async
+
     public void setRecord() {
         soundRecorder.startRecord();
     }
